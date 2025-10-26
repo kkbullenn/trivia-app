8e48b3a3bc9ac971c2779cc6a7d82ca457797bf6
Li-Yang-CS <kavenli23@gmail.com>
2025-10-26 12:07:31 -0700

    added a BE logic to display quiz results

--- WEB-INF/classes/ExitServlet.java
+++ WEB-INF/classes/ExitServlet.java
@@ -3,8 +3,11 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/EXIT")
-public class ExitServlet extends HttpServlet {
-    @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+public class ExitServlet extends HttpServlet
+{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+    {
         HttpSession s = req.getSession(false);
         if (s != null) s.invalidate();
         resp.sendRedirect("/login"); // or "/"
