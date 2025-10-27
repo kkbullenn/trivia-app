73cea8e49b5a84f293fe0720e8048e7a36848903
LEGACY21\tkngu <tknguyen2111@gmail.com>
2025-10-27 12:13:44 -0700

    Add QuizServlet for sending quiz html page to browser

---
+++ src/main/java/com/triviaapp/servlets/QuizServlet.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/active-lobby/quiz")
+public class QuizServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException, ServletException {
+        
+        // Check if user is logged in
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user_id") == null) {
+            response.sendRedirect("login");
+            return;
+        } else if (session.getAttribute("lobby_id") == null) {
+            response.sendRedirect("category-lobbies");
+            return;
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/quiz.html");
+        dispatcher.forward(request, response);
+    }
+    
+}
