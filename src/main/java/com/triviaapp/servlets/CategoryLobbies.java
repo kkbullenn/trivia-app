930bf6ee81081137a250f4bd3c38b9f1d899ff10
LEGACY21\tkngu <tknguyen2111@gmail.com>
2025-10-27 01:14:14 -0700

    Add BE logic to send html page and return JSON data for category lobbies

--- src/main/java/com/triviaapp/servlets/CategoryLobbies.java
+++ src/main/java/com/triviaapp/servlets/CategoryLobbies.java
@@ -1,9 +1,4 @@
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Locale.Category;
-import java.util.Map;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,8 +6,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 @WebServlet("/category-lobbies")
 public class CategoryLobbies extends HttpServlet {
@@ -27,17 +20,8 @@ public class CategoryLobbies extends HttpServlet {
             response.sendRedirect("login");
             return;
         }
-        int userId = (Integer) session.getAttribute("user_id");
 
-        // Create Session DAO object to grab all available sessions from database
-        SessionDAO sessionDAO = new SessionDAOImpl();
-        List<Map<String, String>> sessions;
-        try {
-            sessions = sessionDAO.findSessionsByHost(userId);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database error");
-            return;
-        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/category-lobbies.html");
+        dispatcher.forward(request, response);
     }
 }
