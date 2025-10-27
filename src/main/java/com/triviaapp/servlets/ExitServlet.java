ba6cb95ae64e3e660b8e307ffc89caf66c09c0de
LEGACY21\tkngu <tknguyen2111@gmail.com>
2025-10-27 01:42:16 -0700

    Resolve conflict

--- src/main/java/com/triviaapp/servlets/ExitServlet.java
+++ src/main/java/com/triviaapp/servlets/ExitServlet.java
@@ -1,40 +1,23 @@
-<<<<<<< HEAD
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
 import java.io.IOException;
-
-@WebServlet("/EXIT")
-public class ExitServlet extends HttpServlet
-{
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
-    {
-=======
-package your.package;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.io.IOException;
 
 @WebServlet("/EXIT")
 public class ExitServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
->>>>>>> dev
         HttpSession s = req.getSession(false);
         if (s != null) s.invalidate();
         resp.sendRedirect("/login"); // or "/"
     }
-<<<<<<< HEAD
-=======
 
     // Optional: also handle GET (in case someone links to /EXIT)
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         doPost(req, resp);
     }
->>>>>>> dev
 }
