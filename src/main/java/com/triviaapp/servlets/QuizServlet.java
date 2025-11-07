bc68ec07ff09a605407ac83c952a358afe943de7
LEGACY21\tkngu <tknguyen2111@gmail.com>
2025-11-07 12:56:17 -0800

    Remove redundant mapping in servlet and add package

--- src/main/java/com/triviaapp/servlets/QuizServlet.java
+++ src/main/java/com/triviaapp/servlets/QuizServlet.java
@@ -1,20 +1,20 @@
+package com.triviaapp.servlets;
+
 import java.io.IOException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/quiz")
 public class QuizServlet extends HttpServlet {
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException, ServletException {
-        
+
         // Check if user is logged in and has joined a lobby
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("user_id") == null) {
@@ -31,5 +31,5 @@ public class QuizServlet extends HttpServlet {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/quiz.html");
         dispatcher.forward(request, response);
     }
-    
+
 }
