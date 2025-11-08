54a76f3ceaa6a4616db6affd90e9300f63589ede
LEGACY21\tkngu <tknguyen2111@gmail.com>
2025-11-08 00:46:48 -0800

    Modify Admin and AdminData servlets to only allow user with role admin and return both category data and session data

--- src/main/java/com/triviaapp/servlets/AdminServlet.java
+++ src/main/java/com/triviaapp/servlets/AdminServlet.java
@@ -1,26 +1,27 @@
+package com.triviaapp.servlets;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.WebServlet;
 import java.io.*;
-import java.sql.*;
 
-
-public class CreateQuizServlet extends HttpServlet {
-
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/quizapp";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "password"; // change this when server is set up
+public class AdminServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+        
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user_id") == null) {
+            // Not logged in, redirect to  login page
+            response.sendRedirect("login");
+            return;
+        } else if (!"admin".equals(session.getAttribute("role_name"))) {
+            // Not an admin, redirect to main page
+            response.sendRedirect("main");
+            return;
+        }
+
         RequestDispatcher dispatcher = request.getRequestDispatcher("/admin_landing.html");
         dispatcher.forward(request, response);
     }
-
-@Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-    }
-
 }
